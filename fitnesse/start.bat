1a48651e935d0936a79e5ccf786f55c6b91e0a62
Hans Reier Sigmond <hans.reier.sigmond@ciber.no>
2019-09-28 12:18:29 +0200

    endret bat fil

--- fitnesse/start.bat
+++ fitnesse/start.bat
@@ -1,2 +1,2 @@
 cd c:/webServer/fitnesse
-java -jar fitnesse-standalone.jar -p 8080 -e 0 -d c:/git/work/fitnesse -l C:/fitnesseLogs
+java -jar fitnesse-standalone.jar -p 8080 -e 0 -d c:/git/fitnesse/fitnesse -l C:/fitnesseLogs
