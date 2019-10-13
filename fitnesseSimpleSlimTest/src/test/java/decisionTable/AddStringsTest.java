bc9fef82cb75d6e5479e10a1ee3cfd34c0a7d95b
Hans Reier Sigmond <hans.reier.sigmond@ciber.no>
2019-10-13 15:29:16 +0200

    sjekket inn en gang til etter feil revert

--- fitnesseSimpleSlimTest/src/test/java/decisionTable/AddStringsTest.java
+++ fitnesseSimpleSlimTest/src/test/java/decisionTable/AddStringsTest.java
@@ -1,11 +1,18 @@
 package decisionTable;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AddStringsTest {
 
     private AddStrings addStrings = new AddStrings();
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("java.util.logging.config.file",
+                ClassLoader.getSystemResource("logging.properties").getPath());
+    }
 
     @Test
     public void AddStringsTest() {
--- fitnesseSimpleSlimTest/src/test/java/decisionTable/MultiplicationTest.java
+++ fitnesseSimpleSlimTest/src/test/java/decisionTable/MultiplicationTest.java
@@ -1,9 +1,18 @@
 package decisionTable;
 
 import static junit.framework.Assert.*;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MultiplicationTest {
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("java.util.logging.config.file",
+                ClassLoader.getSystemResource("logging.properties").getPath());
+    }
+
     @Test
     public void test() {
         Multiplication multiplication = new Multiplication();
