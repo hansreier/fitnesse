c0b94661a928cbf12120dbddf065f02e23ddeeca
Hans Reier Sigmond <reier.sigmond@gmail.com>
2021-04-14 16:02:09 +0200

    lagt inn encoding, oppdatert pom til Java 11

--- car/src/main/java/places/Location.java
+++ car/src/main/java/places/Location.java
@@ -3,32 +3,33 @@ package places;
 //Eclipse formatting turned off.
 //@formatter:off
 public enum Location {
-	NOPLACE(""),
-	OSLO("Oslo"), 
-	LILLEHAMMER("Lillehammer"), 
-	HAMAR("Hamar"), 
-	GJOVIK("Gjøvik"), 
-	MOELV("Moelv");
+    NOPLACE(""),
+    OSLO("Oslo"),
+    LILLEHAMMER("Lillehammer"),
+    HAMAR("Hamar"),
+    GJOVIK("Gjï¿½vik"),
+    MOELV("Moelv");
 
-	Location(String name) {
-		this.name = name;
-	};
+    Location(String name) {
+        this.name = name;
+    }
 
-	private String name; // operator abbreviation/keyword
-	public String getName() {
-		return name;
-	}
+    private String name; // operator abbreviation/keyword
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public static Location get(String name) {
-		for (Location l : Location.values()) {
-			if (l.getName().toLowerCase().equals(name.toLowerCase())) {
-				return l;
-			}
-		}
-		return NOPLACE;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static Location get(String name) {
+        for (Location l : Location.values()) {
+            if (l.getName().equalsIgnoreCase(name)) {
+                return l;
+            }
+        }
+        return NOPLACE;
+    }
 }
